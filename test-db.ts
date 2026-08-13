a68aa51b127d76141804f1be1a89f8dbeb373820
fountainfang <119987045+fountainfang@users.noreply.github.com>
2026-08-13 12:10:48 +0700

    chore: ignore .npm-cache and .vscode directories in docker and git configurations

--- test-db.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-async function main() {
-  const p = await prisma.product.findFirst({ where: { title: 'Otello' } })
-  console.log(p)
-}
-main().catch(console.error).finally(() => prisma.$disconnect())
